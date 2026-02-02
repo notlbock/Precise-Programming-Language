306ba7b6e32c049a32a8bcb2b4593da0c0c294df
E <uyenla1224@gmail.com>
2026-02-02 15:56:09 -0800

    Update Precise.c

--- Precise.c
+++ Precise.c
@@ -3,6 +3,8 @@
 #include "h/pver.h"// since when we compile using -o and -l in the further stage this will be in
 #include "h/help.h"
 #include "h/unist.h"
+#include "h/doc.h"
+#include "h/cmpi.h"
 // /usr/local/bin and this folder will be in /opt
 
 // lets add some elses
@@ -13,6 +15,10 @@ int main(int argc, char* argv[]){
 			prver();
 		}else if(strcmp(arg1, "--uninstall") == 0){
 			uninstall(); // lets hope for no errors
+		}else if(strcmp(arg1, "--doc") == 0){
+			hdoc(argv[2]);
+		}else if(strcmp(arg1, "--build") == 0){
+			cmpil(argv[2]);
 		}else{
 			printf("No such argument: %s", arg1);
 			return 1;
