5e72c767311434c6d71dad866805963bb66abc1c
E <uyenla1224@gmail.com>
2026-02-01 20:21:10 -0800

    Add files via upload

---
+++ help.h
@@ -0,0 +1,12 @@
+#ifndef HELP_H
+#define HELP_H
+
+#include <stdio.h>
+
+// lets not make the same mistake
+static inline void help(){
+	printf("HELP:\n");
+	printf("--version: Prints the version of Precise");
+}
+
+#endif // HELP_H
